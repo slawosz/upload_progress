001b50393446bcdd0b0f083049754f8f4935e4e3
Sławosz <slawosz@gmail.com>
2012-08-12 19:58:22 +0200

    implement progress data manager

--- lib/upload_progress/progress_data_manager.rb
+++ lib/upload_progress/progress_data_manager.rb
@@ -1,4 +1,20 @@
+require 'redis'
+
 module UploadProgress
   class ProgressDataManager
+
+    def initialize(uid)
+      @uid = uid
+      @redis = Redis.new
+    end
+
+    def save(value)
+      @redis.set(@uid.to_s, value)
+    end
+
+    def get
+      @redis.get(@uid.to_s)
+    end
+    
   end
 end

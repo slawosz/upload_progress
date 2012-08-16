fef48558d902581f0e51da361f328e6d51f1cc37
Sławosz <slawosz@gmail.com>
2012-08-16 13:13:50 +0200

    return strings

--- lib/upload_progress/check_progress.rb
+++ lib/upload_progress/check_progress.rb
@@ -3,8 +3,8 @@ module UploadProgress
 
     def call(env)
       uid = UidParser.new(env['QUERY_STRING']).parse
-      progress = ProgressDataManager.new(uid).get
-      [200, {'Date' => Time.now.to_s, 'Content-Length' => progress.to_s.length, 'Content-Type' => 'text/html'}, progress]
+      progress = ProgressDataManager.new(uid).get.to_s
+      [200, {'Date' => Time.now.to_s, 'Content-Length' => progress.length.to_s, 'Content-Type' => 'text/html'}, progress]
     end
       
   end

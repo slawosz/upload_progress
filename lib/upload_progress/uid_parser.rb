6999f54e690f4bcadd8bfe3aca47e14c2b51015b
Sławosz <slawosz@gmail.com>
2012-08-19 23:18:57 +0200

    fix uid parser

--- lib/upload_progress/uid_parser.rb
+++ lib/upload_progress/uid_parser.rb
@@ -1,13 +1,20 @@
 module UploadProgress
   class UidParser
 
-    def initialize(uid)
-      @uid = uid
+    def initialize(query_string)
+      @query_string = query_string
     end
 
     def parse
-      return nil unless @uid
-      @uid.match(/uid=(\d+)/)[1]
+      return nil if uid_cannot_be_obtained?
+      @query_string.match(/uid=(\d+)/)[1]
     end
+
+    private
+
+    def uid_cannot_be_obtained?
+      @query_string == nil || @query_string !~ /uid=\d+/
+    end
+    
   end
 end
--- spec/uid_parser_spec.rb
+++ spec/uid_parser_spec.rb
@@ -6,8 +6,14 @@ describe UploadProgress::UidParser do
     up.parse.should == '666'
   end
 
-  it 'shoud return nil when @uid is nil' do
+  it 'shoud return nil when query string is nil' do
     up = UploadProgress::UidParser.new(nil)
     up.parse.should == nil
   end
+
+  it 'should return nil when query string does not contain uid' do
+    up = UploadProgress::UidParser.new('foo=bar')
+    up.parse.should == nil
+  end
+  
 end

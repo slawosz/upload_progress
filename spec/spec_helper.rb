1cb727dfdf0e4b9968939bc3714cdb76bec00ce1
Sławosz <slawosz@gmail.com>
2012-08-20 22:32:11 +0200

    missing helper method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,20 @@ class FakeManager
   def get; end
 end
 
+# proudly stolen from https://github.com/rails/rails/blob/9e0b3fc7cfba43af55377488f991348e2de24515/activesupport/lib/active_support/inflector/methods.rb#L213
+def constantize(constant_string)
+  names = constant_string.split('::')
+  names.shift if names.empty? || names.first.empty?
+
+  constant = Object
+  names.each do |name|
+    constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+  end
+  constant
+end
+
 require_relative '../lib/upload_progress.rb'
 require 'rspec'
+
+
 

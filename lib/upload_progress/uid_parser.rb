7d456c9d04aebc507cb61f532e819927989a7595
Sławosz <slawosz@gmail.com>
2012-09-30 22:41:43 +0200

    change UidParser api

--- lib/upload_progress/uid_parser.rb
+++ lib/upload_progress/uid_parser.rb
@@ -1,18 +1,15 @@
 module UploadProgress
   class UidParser
 
-    def initialize(query_string)
+    def self.parse(query_string)
       @query_string = query_string
-    end
-
-    def parse
       return nil if uid_cannot_be_obtained?
       @query_string.match(/uid=(\d+)/)[1]
     end
 
     private
 
-    def uid_cannot_be_obtained?
+    def self.uid_cannot_be_obtained?
       @query_string == nil || @query_string !~ /uid=\d+/
     end
     
--- spec/uid_parser_spec.rb
+++ spec/uid_parser_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe UploadProgress::UidParser do
   it 'should get uid' do
-    up = UploadProgress::UidParser.new('?uid=666')
-    up.parse.should == '666'
+    up = UploadProgress::UidParser.parse('?uid=666')
+    up.should == '666'
   end
 
   it 'shoud return nil when query string is nil' do
-    up = UploadProgress::UidParser.new(nil)
-    up.parse.should == nil
+    up = UploadProgress::UidParser.parse(nil)
+    up.should == nil
   end
 
   it 'should return nil when query string does not contain uid' do
-    up = UploadProgress::UidParser.new('foo=bar')
-    up.parse.should == nil
+    up = UploadProgress::UidParser.parse('foo=bar')
+    up.should == nil
   end
   
 end
